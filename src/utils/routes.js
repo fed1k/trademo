ed966400f2b350798b627df9bf87b4890265393b
fed1k <allamurotovcoder@mail.ru>
2025-02-11 21:34:42 +0500

    design home page desktop

---
+++ src/utils/routes.js
@@ -0,0 +1,47 @@
+export const routes = [
+    {
+        text: "Главная",
+        icon: "",
+        href: "/home"
+    },
+    {
+        text: "Сделки",
+        icon: "",
+        href: "/deals"
+    },
+    {
+        text: "Выплаты",
+        icon: "",
+        href: "/withdrawals"
+    },
+    {
+        text: "Споры",
+        icon: "",
+        href: "/disputes"
+    },
+    {
+        text: "Профили",
+        icon: "",
+        href: "/bank-profiles"
+    },
+    {
+        text: "Реквизиты",
+        icon: "",
+        href: "/requisites"
+    },
+    {
+        text: "Устройства",
+        icon: "",
+        href: "/devices"
+    },
+    {
+        text: "Финансы",
+        icon: "",
+        href: "/finances"
+    },
+    {
+        text: "Сообщения",
+        icon: "",
+        href: "/messages"
+    }
+]
